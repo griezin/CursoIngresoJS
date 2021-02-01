a3d227ccbcdd41c89a565e8cb5fe5f05aad1e936
griezin <facu.facu811@gmail.com>
2021-02-01 11:09:57 -0600

    creo que ya hice esto demasiadas veces

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola , ya esta!*");
+	alert("Otra vez , lo mismo pero prueba 550");
 }
 
